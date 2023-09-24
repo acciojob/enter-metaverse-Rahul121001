e90a63610cb9abd0b56bd452e79f59475049edf1
Rahul121001 <139845999+Rahul121001@users.noreply.github.com>
2023-09-24 20:31:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const metaverseStatus = document.querySelector("#metaverse-status");
+const enterMetaverseButton = document.querySelector("#enter-metaverse-button");
+
+enterMetaverseButton.addEventListener("click", () => {
+  metaverseStatus.textContent = "Entered Metaverse";
+});
